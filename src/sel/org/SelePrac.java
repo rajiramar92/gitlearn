96a0b5f56c733f609c4c3e49d9ec65a877e2e1dc
joysaravana <joysaravana94@gmail.com>
2022-05-02 13:54:29 +0530

    project added

---
+++ src/sel/org/SelePrac.java
@@ -0,0 +1,118 @@
+package sel.org;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
+
+import javax.tools.Tool;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelePrac {
+	public static void main(String[] args) {
+		int count=1;
+        String a="selvi Ramarci shaish u";
+		 char[] ch = a.toCharArray();
+		 for (int i = 0; i < ch.length; i++) {
+			 if (a.charAt(i)==' ' && a.charAt(i+1)!=' ') {
+				count++;
+			}
+
+			}
+			System.out.println(count);
+		}
+        
+        
+        
+        
+        
+			
+		}
+  
+       
+		
+		
+	
+
+	
+	
+		
+		
+	
+
+		
+		
+	
+		
+		
+	
+	
+
+	
+			
+		
+           
+		
+
+			
+		
+			
+		
+		
+	
+	
+
+
+		
+		
+	
+	
+	
+	
+
+	
+
+
+		
+		
+
+
+		
+			
+		
+	
+
+
+
+
